00cc21eff832b147d539a4fb7314c47c4b3d8da6
abhishek1105 <42262286+abhishek1105@users.noreply.github.com>
2018-10-03 10:26:38 +0530

    Update inputarray.java

--- Assignment 1/inputarray.java
+++ Assignment 1/inputarray.java
@@ -2,6 +2,7 @@ import java.util.*;
 class inputarray {
   public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
+	  int maximum;
 	System.out.println("ENTER SIZE");
 	int size=sc.nextInt();
     int[] arr=new int[size];
@@ -16,6 +17,17 @@ class inputarray {
     {
       System.out.println(arr[i]);
     }
+	   maximum = arr[0];
+ 
+  for (int c = 1; c < size; c++)
+  {
+    if (arr[c] > maximum)
+    {
+       maximum  = arr[c];
+       
+    }
+  }
+  System.out.println("MAXIMUM number is :"+maximum);
     
   }
 }
